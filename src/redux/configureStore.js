9b83840bd2ba389683ef33b6dd76a6f08fc232da
adelguit <adeelguitoun@gmail.com>
2022-11-15 16:22:38 +0100

    Fix eslint errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,7 @@ import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 
 // Slices
-import dragonsSlice from './dragons/dragonsSlice.js';
+import dragonsSlice from './dragons/dragons';
 
 // Combine slices reducer
 const rootReducer = {
