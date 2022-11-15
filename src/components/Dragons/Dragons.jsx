ff650aa7d60855168b05e850b57451575d96b544
adelguit <adeelguitoun@gmail.com>
2022-11-15 15:16:41 +0100

    Fetch and display data

--- src/components/Dragons/Dragons.jsx
+++ src/components/Dragons/Dragons.jsx
@@ -1,22 +1,35 @@
-// Component
+// React hooks
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// Actions Creators
+import { fetchDragons } from "../../redux/dragons/dragonsSlice";
+// Components
 import DragonItem from "./DragonItem";
 
 const Dragons = () => {
-  const dragons = [];
+  const dragons = useSelector(state => state.dragons.dragons);
+  const dispatch = useDispatch();
+
+  useEffect(()=>   {
+    dispatch(fetchDragons())
+  }, [])
 
   return(
-    <ul className="dragons-list">
-      {
-        dragons.map((dragon) => {
-          <DragonItem 
-            key={dragon.id}
-            name={dragon.name}
-            type={dragon.type}
-            img={dragon.flickr_images}
-          />
-        })
-      }
-    </ul>
+    <div className="container" style={{"border": "none"}}>
+      <ul className="dragons-list">
+        {
+          dragons.map((dragon) => (
+              <DragonItem 
+                key={dragon.id}
+                name={dragon.name}
+                type={dragon.type}
+                description={dragon.description}
+                img={dragon.flickr_images}
+              />
+            ))
+        }
+      </ul>
+    </div>
   )
 }
 
